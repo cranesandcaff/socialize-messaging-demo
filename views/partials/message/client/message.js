2feb276adfb586229ac97a63b68ee238330ece65
Kelly <copleykj@gmail.com>
2015-05-21 15:24:23 -0400

    add feedback to incoming messages

--- views/partials/message/client/message.js
+++ views/partials/message/client/message.js
@@ -2,6 +2,12 @@ window.scrollEnd = function (container){
     container.prop({scrollTop:container.prop("scrollHeight")});
 };
 
+Feedback.profiles = {
+    "newMessage": {
+        sound: "https://soundkit.io/samples/Affirmative.mp3",
+    }
+}
+
 Template.message.rendered = function () {
 
 
@@ -11,16 +17,21 @@ Template.message.rendered = function () {
     var innerHeight = messageContainer.innerHeight();
     var scrollTop = messageContainer.scrollTop();
     var difference = scrollHeight - scrollTop - innerHeight;
+    var scrolled = state.get("scrolled");
 
     if(scrollHeight > innerHeight){
-        if(!state.get("scrolled") || difference <= 10){
+        var message = Meteor.messages.findOne({conversationId:this.data.conversationId}, {sort:{date:-1}});
+        var messageId = message && message._id;
 
-//                if(CurrentConversationInstance.scrolled){
-//                    window.newMessageSound.play();
-//                }
+        if(this.data._id === messageId){
+            if(!scrolled || difference <= 10){
+                if(scrolled && message && message.userId){
+                    Feedback.provide("newMessage");
+                }
 
-            state.set("scrolled", true);
-            window.scrollEnd(messageContainer);
+                state.set("scrolled", true);
+                window.scrollEnd(messageContainer);
+            }
         }
     }
 
